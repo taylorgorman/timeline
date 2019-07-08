cc46c83baefe8bf650eb0fa31eab9818740d550a
Taylor Gorman <me@taylorpatrickgorman.com>
2019-07-08 14:01:38 -0400

    👷‍♂️ woa, a lot has happened

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,72 +1,166 @@
 import React from 'react';
 import moment from 'moment';
 
+moment.prototype.daysInYear = function(){
+  return this.isLeapYear() ? 366 : 365;
+}
+
 export default class Timeline extends React.Component {
 
   state = {
-    items: [
+    items: []
+  }
+
+  widthMultiplier = .3
+
+  render() { console.log('render()');
+
+    // timeline boundaries
+    const startDate = moment.min( this.state.items.map( item => item.start ) )
+    const startYear = startDate.clone().startOf('year')
+    const startMonth = startDate.clone().startOf('month')
+    const endDate = moment.max( this.state.items.map( item => item.end ) )
+    const endYear = endDate.clone().startOf('year')
+    const endMonth = endDate.clone().startOf('month')
+
+    // collect years
+    let years = []
+    let iYear = startYear.clone()
+    while ( moment.min( iYear, endDate ) === iYear ) {
+
+      //console.log(`iYear`, iYear.format('MM DD YYYY'));
+      //console.log(`startYear`, startYear.format('MM DD YYYY'));
+      //console.log(`endYear`, endYear.format('MM DD YYYY'));
+
+      // All middle years
+      let daysIn = iYear.daysInYear()
+
+      // Is first year
+      if ( iYear.isSame( startYear ) ) {
+        daysIn = startYear.clone().endOf('year').diff( startMonth, 'd' ) + 1
+        console.log('is first year', daysIn);
+      }
+
+      // Is last year
+      if ( iYear.isSame( endYear ) ) {
+        daysIn = endMonth.clone().endOf('month').diff( endYear, 'd' ) + 1
+        console.log('is last year', daysIn);
+      }
+
+      // Save
+      years.push({
+        moment: iYear.clone(),
+        daysIn,
+      });
+
+      // Increment
+      iYear.add( 1, 'Y' );
+    }
+
+    // collect months
+    let months = [];
+    let iMonth = startMonth.clone();
+    while ( moment.min( iMonth, endDate ) === iMonth ) {
+      months.push( iMonth.clone() );
+      iMonth.add( 1, 'M' );
+    }
+
+    // calculate extra data from items
+    const itemsProcessed = this.state.items.map( item => {
+      const daysLength = item.end.diff( item.start, 'd' )
+      const daysSinceStart = item.start.diff( startDate, 'd' )
+      return {
+        ...item,
+        daysLength,
+        daysSinceStart,
+        style: {
+          width: ( daysLength + 1 ) * this.widthMultiplier + 'rem', // first and last days inclusive
+          marginLeft: daysSinceStart * this.widthMultiplier + 'rem',
+        },
+      }
+    } )
+
+    // render marks for days of month
+    const renderMarks = ( n ) => {
+      let marks = [];
+      for ( let i = 0; i < n; i++ ) {
+        marks.push( <div className="month-mark" style={{ width: 'calc(' + this.widthMultiplier + 'rem - 1px)' }} key={ i } /> )
+      }
+      return marks;
+    }
+
+    // init year vars
+    let thisYear = startDate.clone().startOf( 'year' )
+    let lastYear = thisYear.subtract( 1, 'year' )
+    let showYear = true
+    let yearDaysLength = 0
+
+    console.log('years before render()',years);
+    return (
+
+      <div className={ 'timeline' + ( this.widthMultiplier < .5 ? ' no-day-marks' : '' ) }>
+
+        { itemsProcessed.map( ( item, key ) => (
+          <div className="item" style={ item.style } key={ key }>
+            <span className="title">
+              { item.title }
+            </span>
+          </div>
+        ) ) }
+
+        <div className="time-line">
+          <div className="years">
+          { years.map( ( year, key ) => (
+            <div className="year" style={{ width: year.daysIn * this.widthMultiplier + 'rem' }} key={key}>
+              <span className="name">{ year.moment.format('YYYY') }</span>
+            </div>
+          ) ) }
+          </div>
+          <div className="months">
+          { months.map( ( month, key ) => (
+            <div className="month" key={key}>
+
+              <div className="month-marks">
+                { renderMarks( month.daysInMonth() ) }
+              </div>
+              <div className="month-name">
+                { month.format('MMM') }
+              </div>
+            </div>
+          ) ) }
+          </div>
+        </div>
+
+      </div>
+
+    )
+  }
+
+  componentDidMount() { console.log('componentDidMount()');
+
+    let items = [
       {
         type: 'person',
         title: 'Jason',
-        start: moment('2018-09-01'),
-        end: moment('2019-04-01'),
+        start: moment('2018-09-07'),
+        end: moment('2019-02-10'),
       },
       {
         type: 'person',
         title: 'Alex',
-        start: moment('2019-01-01'),
-        end: moment('2019-08-01'),
+        start: moment('2019-01-22'),
+        end: moment('2019-08-13'),
       },
       {
         type: 'person',
         title: 'Marsha',
-        start: moment('2019-03-01'),
-        end: moment('2019-07-01'),
+        start: moment('2018-11-21'),
+        end: moment('2019-07-03'),
       },
-    ],
-  }
+    ]
 
-  startDate = moment.min( this.state.items.map( item => item.start ) )
-  endDate = moment.max( this.state.items.map( item => item.end ) )
+    this.setState({items});
 
-  widthMultiplier = .3
-
-  itemStyle = ( item ) => ({
-    width: item.end.diff( item.start, 'd' ) * this.widthMultiplier + 'em',
-    marginLeft: item.start.diff( this.startDate, 'd' ) * this.widthMultiplier + 'em',
-  })
-
-  render() {
-
-    let months = [];
-    let iMonth = this.startDate;
-    while ( moment.min( iMonth, this.endDate ) === iMonth ) {
-      months.push({
-        name: iMonth.format('MMM YYYY'),
-        daysIn: iMonth.daysInMonth(),
-      });
-      iMonth.add( 1, 'M' );
-    }
-
-    return (
-
-    <div className="timeline">
-      { this.state.items.map( ( item, key ) => { console.log('this.itemStyle(item)',this.itemStyle(item)); return (
-        <div className="item" style={ this.itemStyle(item) } key={key}>
-          <span className="title">
-            { item.title }
-          </span>
-        </div>
-      ) } ) }
-
-      <div className="time-line">
-      { months.map( ( month, key ) => (
-        <div className="month" style={{ width: month.daysIn * this.widthMultiplier + 'em' }} key={key}>{ month.name }</div>
-      ) ) }
-      </div>
-    </div>
-
-    )
   }
 
 }
